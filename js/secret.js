a9673b244dfe6bd7e9195871188945fe19296ae4
Harry Venables <venablesh@yahoo.co.uk>
2015-10-20 12:14:26 +0100

    added mock to our tests

---
+++ js/secret.js
@@ -0,0 +1,1 @@
+var access_token = '6d28e87b83ef495b9c408626824496796a1bd3bd'
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -14,21 +14,32 @@ describe('GitUserSearchController', function() {
 
   describe('when searching for a user', function() {
     var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
+    {
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    },
+    {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }
+  ];
+
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+      .when("GET", "https://api.github.com/search/users" + "?access_token=" + access_token + "&q=hello")
+      .respond(
+        { items: items }
+      );
+    }));
 
     it('displays search results', function() {
       ctrl.searchTerm = "hello";
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
